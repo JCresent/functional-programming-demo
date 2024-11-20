6c70fa646f919eb63d67da0342f9880bd9929348
Justin Cresent <JCresent@hotmail.com>
2024-11-20 13:21:18 -0500

    Finished

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,34 @@
 -- Write printAMessage here
+printAMessage :: Show a => a -> IO ()
+printAMessage x = putStrLn (show x)
 
 -- Write division here
+division :: Double -> Double -> Maybe Double
+division _ 0 = Nothing
+division x y = Just (x / y)
 
 -- Write factorial here
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
 
 -- Write factList here
+factList :: Int -> [Int]
+factList n = [factorial x | x <- [0..n]]
 
 -- Write merge here
+merge :: [Int] -> [Int] -> [Int]
+merge [] [] = []
+merge [] ys = ys
+merge xs [] = xs
+merge (x : xs) (y : ys)
+    | x < y = x : merge xs (y : ys)
+    | otherwise = y : merge (x : xs) ys
 
-main = print "Test" -- Replace this with your testing code
+main = print(merge [1, 3, 5] [2, 4, 6])
+    
+    
+    -- TEST DIVISION
+    -- main = case division 5 0 of
+        -- Nothing -> putStrLn "Division by zero"
+        -- Just result -> printAMessage result
--- src/test.hs
+++ src/test.hs
@@ -1,2 +1,19 @@
 -- Feel free to experiment with Haskell in this file!
 -- This will not affect your assignment's grade
+addNums :: Int -> Int
+addNums 0 = 0
+addNums n = n + addNums (n - 1)
+
+
+-- Add values taken to the given power of each number in the list 
+sumPowers :: Int -> [Int] -> Int
+sumPowers _ [] = 0
+sumPowers power (x : xs) = (x ^ power) + sumPowers power xs
+
+sumPowers2 = sumPowers 2
+sumPowers3 = sumPowers 3
+
+sum1 = sumPowers2 [1, 2, 3, 4] -- 30
+sum2 = sumPowers3 [1, 2, 3, 4] -- 10
+
+main = print (sum1, sum2)
